6542c0c0f74a5df255eb502079bf922c7486915f
werdl <werdl_@outlook.com>
2023-03-24 17:02:28 +0000

    cmit

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 i686-elf-gcc -c kernel/kernel.c -o kernel/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T boot/linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot/boot.o kernel/kernel.o -lgcc
-qemu-system-i386 -kernel myos.bin
+i686-elf-gcc -T boot/linker.ld -o os.bin -ffreestanding -O2 -nostdlib boot/boot.o kernel/kernel.o -lgcc
+qemu-system-i386 -kernel os.bin
 rm -rf kernel/kernel.o
-rm -rf myos.bin
+rm -rf os.bin
