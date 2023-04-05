c189f3e164ab67fadce4dc18e43e6d2141f874af
werdl <werdl_@outlook.com>
2023-04-05 07:12:27 +0100

    computer v computer working

--- games/main.c
+++ games/main.c
@@ -57,6 +57,9 @@ bool checkwin() {
     else if ((p[0]==0&&p[3]==0&&p[6]==0) || (p[0]==1&&p[3]==1&&p[6]==1)) return true; //left column
     else if ((p[1]==0&&p[4]==0&&p[7]==0) || (p[1]==1&&p[4]==1&&p[7]==1)) return true; //middle column
     else if ((p[2]==0&&p[5]==0&&p[8]==0) || (p[2]==1&&p[5]==1&&p[8]==1)) return true; //right column
+
+    else if (((p[0]==0&&p[4]==0&&p[8]==0) || (p[0]==1&&p[4]==1&&p[8]==1))) return true; //TL to BR
+    else if (((p[2]==0&&p[4]==0&&p[6]==0) || (p[2]==1&&p[4]==1&&p[6]==1))) return true;
     else return false;
 }
 char * getInput() {
@@ -69,6 +72,18 @@ char * getInput() {
         return inp;
     }
 }
+void win(int move) {
+    if (move==1) {
+        printf("Victory for Noughts O");
+    } else if (move==0) {
+        printf("Victory for the crosses X");
+    } else if (move==-1) {
+        printf("Nobody wins.");
+    }else {
+        return -1;
+    }
+    return 0;
+}
 int main(int argc, char ** argv) {
     srand(time(NULL));
     render(3,3,3,3,3,3,3,3,3);
@@ -76,42 +91,56 @@ int main(int argc, char ** argv) {
     // render(p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8]);
     
     int move=0;
-
+    bool faile=false;
     while (1) {
+        whil:
+        
+        if (faile==true) {
+            faile=false;
+        } else {
+            tries++;
+        }
         move=tries%2;
-        tries++;
         char * inp;
+        if (checkwin()==true) {
+            win(move);
+            break;
+        }
+        
         if (tries>9) {
+            win(-1);
             return 0;
         } else {
             inp=getInput();
         }
-        // printf("%d",tries);
-        if (checkwin()==true) break;
+        
         
         if(inp=="tl" && p[0]==3){
             p[0]=move;
-        } if(inp== "tm" && p[1]==3){
+        } else if(inp== "tm" && p[1]==3){
             p[1]=move;
-        } if(inp== "tr" && p[2]==3){
+        } else if(inp== "tr" && p[2]==3){
             p[2]=move;
-
-        } if(inp== "ml" && p[3]==3){
+        } else if(inp== "ml" && p[3]==3){
             p[3]=move;
-        } if(inp== "mm" && p[4]==3){
+        } else if(inp== "mm" && p[4]==3){
             p[4]=move;
-        } if(inp== "mr" && p[5]==3){
+        } else if(inp== "mr" && p[5]==3){
             p[5]=move;
-
-        } if(inp== "bl" && p[6]==3){
+        } else if(inp== "bl" && p[6]==3){
             p[6]=move;
-        } if(inp== "bm"&& p[7]==3){
+        } else if(inp== "bm"&& p[7]==3){
             p[7]=move;
-        } if(inp== "br"&& p[8]==3){
+        } else if(inp== "br"&& p[8]==3){
             p[8]=move;
+        } else {
+            faile=true;
+            goto whil;
         }
         
         render(p[0],p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8]);
+        // printf("%s",inp);
+        // printf("\n");
     }
     
 }
