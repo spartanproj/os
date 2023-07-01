ae593fa32802764ae5cfea07b7b620fca7d62280
me me <106704354+thisiscoding1234@users.noreply.github.com>
2023-07-01 18:51:43 +0100

    Update donut.c

--- games/donut.c
+++ games/donut.c
@@ -4,22 +4,55 @@
     Target: x86 32bit
     Info: github.com/spartanproj/os
 */
-i,j,k,x,y,o,N;
-main(){float z[1760],a
-#define R(t,x,y) f=x;x-=ty
-;y+=tf;f=(3-xx-yy)/2;x*=f;y*=f;
-=0,e=1,c=1,d=0,f,g,h,G,H,A,t,D;char
-b[1760];for(;;){memset(b,32,1760);g=0,
-h=1;memset(z,0,7040);for(j=0;j<90;j++){
-G=0,H=1;for(i=0;i<314;i++){A=h+2,D=1/(G*
-Aa+ge+5);t=GA e-ga;x=40+30D
-(HAd-tc);y= 12+15D(HAc+
-td);o=x+80y;N =8*((ga-Ghe)
-d-Gha-ge-Hh c);if(22>y&&y>
-0&&x>0&&80>x&&D>z[o]){z[o]=D;b[o]=(N>0
-?N:0)[".,-~:;=!#$@"];}R(.02,H,G);}R(
-.07,h,g);}for(k=0;1761>k;k++)putchar
-(k%80?b[k]:10);R(.04,e,a);R(.02,d,
-c);usleep(15000);printf('\n'+(
-" donut.c! \x1b[23A"));}}
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int A = 0;
+    int B = 0;
+    int width = 80;
+    int height = 24;
+    while (1) {
+        char output[height][width];
+        memset(output, ' ', sizeof(output));
+        double cos_A = cos(A);
+        double sin_B = sin(B);
+        double sin_A = sin(A);
+        double cos_B = cos(B);
+        for (int theta = 0; theta < 628; theta += 2) {
+            double costheta = cos(theta / 100.0);
+            double sintheta = sin(theta / 100.0);
+            for (int phi = 0; phi < 628; phi += 2) {
+                double cosphi = cos(phi / 100.0);
+                double sinphi = sin(phi / 100.0);
+                double circlex = costheta;
+                double circley = 2 + sintheta * cosphi;
+                double circlez = 2 + sintheta * sinphi;
+                circlex = circlex * cos_B - circlez * sin_A * sin_B;
+                circley = circley * cos_A - circlex * sin_A * sin_B - circlez * cos_A * sin_B;
+                circlez = sin_B * circlex + cos_A * sin_A * sin_B * circley + cos_B * circlez - sin_A * sin_B * circlex * circley;
+                circlex = (int)(width / 2 + circlex * width / 8);
+                circley = (int)(height / 2 - circley * height / 8);
+                if (0 <= circlex && circlex < width && 0 <= circley && circley < height) {
+                    int luminance_index = (phi + theta) % 2;
+                    output[circley][circlex] = ".,-~:;=!*#$@"[luminance_index];
+                }
+            }
+        }
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                putchar(output[i][j]);
+            }
+            putchar('\n');
+        }
+        A += 1;
+        B += 2;
+        usleep(100000);
+        system("clear");
+    }
+    return 0;
+}
+
 /*3D-spinning-donut
