ad1ee07ec4b1fba02080bec81883f814e8e154be
me me <106704354+thisiscoding1234@users.noreply.github.com>
2023-07-03 13:04:25 +0000

    NICE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- os_config.sh
+++ os_config.sh
@@ -1,21 +1,34 @@
+RED="\e[31m"
+CYAN="\e[36m"
+GREEN="\e[32m"
+GREY="\e[37m"
+YELLOW="\e[33m"
+BOLD="\e[1m"
+FAINT="\e[2m"
+ITAL="\e[3m"
+UNDER="\e[4m"
+ENDCOLOR="\e[0m"
+
+echo -e "${RED}Red text${ENDCOLOR}"
+
 cd ~
 cd os
 cd initrd
 
-echo --- BlueBerryOS configurator ---
-echo press enter for the defaults
+echo -e "${UNDER} --- BlueBerryOS configurator --- ${ENDCOLOR}"
+echo -e "${BOLD}press enter for the defaults${ENDCOLOR}"
 
 # themes 
-echo -- theme selection --
+echo -e "${ITAL}-- theme selection --${ENDCOLOR}"
 echo what theme would you like? \( check theme guide.md for the option\'s colors and previews of theme\)
 
 echo light or dark?
 read mode
 
 if [ "$mode" = "dark" ]; then
-echo the dark mode options are: BLUE, TERM, PINK, BORE, ANGER, FIRE, RETRO
+echo -e "the dark mode options are: ${CYAN}BLUE, ${GREEN}TERM, ${FAINT}PINK, ${GREY}BORE, ${RED}ANGER, FIRE, ${GREEN}RETRO${ENDCOLOR}"
 read theme
-if [["${theme,,}" = "blue" || "${theme,,}" = "term" || "${theme,,}" = "pink" || "${theme,,}" = "bore" || "${theme,,}" = "anger" || "${theme,,}" = "fire" || "${theme,,}" = "retro" || ]]; then
+if [["${theme,,}" = "blue" || "${theme,,}" = "term" || "${theme,,}" = "pink" || "${theme,,}" = "bore" || "${theme,,}" = "anger" || "${theme,,}" = "fire" || "${theme,,}" = "retro" ]]; then
 "${theme^^}"> color.conf
 fi
 if  ["$theme" = ""]; then
@@ -36,9 +49,13 @@ echo DEAFAULT BLUE\!\!\!\!
 # write to config
 fi
 
+clear
+echo -e "${UNDER} --- BlueBerryOS configurator --- ${ENDCOLOR}"
+echo -e "${BOLD}press enter for the defaults${ENDCOLOR}"
+
 # username
-echo -- User selection --
-echo what username would you like to have? \( root is the root account -- RECOMMENDED ONLY FOR TESTING\!\! \)
+echo -e "${ITAL}-- User selection --${ENDCOLOR}"
+echo -e "what username would you like to have? \( root is the root account -- RECOMMENDED ONLY FOR TESTING\!\! \)"
 read user
 "$user"> user.conf
 
@@ -52,5 +69,7 @@ echo you like to take risks, don\'t you?
 "root"> user.conf
 fi
 
+clear
+echo -e "${UNDER} --- BlueBerryOS configurator --- ${ENDCOLOR}"
 
 echo thank you for using this configurator to make BlueBerryOS work to your preferences!
