a78ced8c4ee6b6edb20143051d763bc347b94765
me me <106704354+thisiscoding1234@users.noreply.github.com>
2023-07-05 15:21:08 +0000

    more bash

--- os_config.sh
+++ os_config.sh
@@ -13,6 +13,11 @@ cd os
 cd initrd
 echo -e "${UNDER} --- BlueBerryOS configurator --- ${ENDCOLOR}"
 echo -e "${BOLD}press enter for the defaults${ENDCOLOR}"
+echo after each menu, it will show what you have selected for 5 seconds
+sleep 5
+clear
+echo -e "${UNDER} --- BlueBerryOS configurator --- ${ENDCOLOR}"
+echo -e "${BOLD}press enter for the defaults${ENDCOLOR}"
 echo -e "${ITAL}-- theme selection --${ENDCOLOR}"
 echo what theme would you like? \( check theme guide.md for the option\'s colors and previews of theme\)
 echo light or dark?
@@ -20,11 +25,11 @@ read mode
 if [ "$mode" = "dark" ]; then
 echo -e "the dark mode options are: ${CYAN}BLUE, ${GREEN}TERM, ${FAINT}PINK, ${GREY}BORE, ${RED}ANGER, FIRE, ${GREEN}RETRO${ENDCOLOR}"
 read theme
-if [["${theme,,}" = "blue" || "${theme,,}" = "term" || "${theme,,}" = "pink" || "${theme,,}" = "bore" || "${theme,,}" = "anger" || "${theme,,}" = "fire" || "${theme,,}" = "retro" ]]; then
-"${theme^^}"> color.conf
+if [ "${theme,,}" = "blue" || "${theme,,}" = "term" || "${theme,,}" = "pink" || "${theme,,}" = "bore" || "${theme,,}" = "anger" || "${theme,,}" = "fire" || "${theme,,}" = "retro" ]; then
+echo "${theme^^}"> color.conf
 fi
-if  ["$theme" = ""]; then
-"BLUE">color.conf
+if  [ "$theme" = ""]; then
+echo "BLUE">color.conf
 fi
 fi
 if [ "$mode" = "light" ]; then
@@ -32,13 +37,14 @@ echo the light mode options are coming soon, so let\'s just go for the default b
 echo you can always edit your terminal colours to be light mode \( or even dark mode! \) as you want with make term
 # this is for later:
 # read theme
-# "${theme^^}"> color.conf
+# echo "${theme^^}"> color.conf
 fi
 if [ "$mode" = "" ]; then
 echo DEAFAULT BLUE\!\!\!\!
-"BLUE"> color.conf
+echo "BLUE"> color.conf
 fi
-clear
+# sleep 5
+# clear
 echo -e "${UNDER} --- BlueBerryOS configurator --- ${ENDCOLOR}"
 echo -e "${BOLD}press enter for the defaults${ENDCOLOR}"
 echo -e "${ITAL}-- User selection --${ENDCOLOR}"
@@ -47,12 +53,13 @@ read user
 "$user" > user.conf
 if [ "$mode" = "" ]; then
 echo YOU\'RE A BLUEBERRY\!\!\!\!
-"blueberry" > user.conf
+echo "blueberry" > user.conf
 fi
 if [ "$mode" = "root" ]; then
 echo you like to take risks, don\'t you? 
-"root"> user.conf
+echo "root"> user.conf
 fi
+sleep 5
 clear
 echo -e "${UNDER} --- BlueBerryOS configurator --- ${ENDCOLOR}"
 echo thank you for using this configurator to make BlueBerryOS work to your preferences!
